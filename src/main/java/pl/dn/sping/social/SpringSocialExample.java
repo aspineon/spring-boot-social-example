8c08005b4fdaf51892c92a33a8258fd5db983e73
Krzysztof Chrusciel <krzysztof.chrusciel@outlook.com>
2017-04-17 20:39:52 +0100

    Initial commit

---
+++ src/main/java/pl/dn/sping/social/SpringSocialExample.java
@@ -0,0 +1,12 @@
+package pl.dn.sping.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialExample {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialExample.class, args);
+	}
+}
